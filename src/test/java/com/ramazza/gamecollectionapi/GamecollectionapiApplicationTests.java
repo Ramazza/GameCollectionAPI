5a4df409ed9f071bd24701a2e0acbb701c1ea628
Ramazza <01ramazza@gmail.com>
2025-04-14 16:33:09 -0300

    First commit

---
+++ src/test/java/com/ramazza/gamecollectionapi/GamecollectionapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramazza.gamecollectionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamecollectionapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
